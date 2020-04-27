8f0763565ba5d6943ead5408d9eceea6d4278422
jodobear <jodobear@protonmail.com>
2020-04-27 17:12:43 +0200

    init

--- fib.py
+++ fib.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
-
-def fib(n):
-	if n <= 1:
-		return 1
-	return (n - 1) + (n + 1)
-
-print(print(5))
+fibSeq = [0,1] 
+  
+def fib(n): 
+    if n < 0: 
+        print("Can't have -ve fibonacci") 
+    elif n <= len(fibSeq):
+        return fibSeq[n-1]
+    else:
+        temp = fib(n-1) + fib(n-2) 
+        fibSeq.append(temp)
+        return temp
+  
+print(f"9th Fibonacci number is: {fib(9)}")
